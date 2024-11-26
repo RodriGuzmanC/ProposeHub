c9fb0a241ec765a0882dcb675827d02c0dd9b2c7
RodrigoG <eduis.carranza123@gmail.com>
2024-11-25 20:44:15 -0500

    se arreglo errores de middleware

--- middleware.ts
+++ middleware.ts
@@ -20,50 +20,55 @@ const publicRoutes = ['/vista/propuesta/login', '/auth/login', '/auth/signin', '
 
 // MIDDLEWARE
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const url = request.nextUrl.clone(); // Clonamos la url
+  try {
+    const { pathname } = request.nextUrl;
+    const url = request.nextUrl.clone(); // Clonamos la url
 
-  // Si la ruta es pública, permite el acceso
-  if (publicRoutes.includes(pathname)) {
-    return NextResponse.next();
-  }
-
-  // Comprobar si la ruta comienza con 'vista/propuesta/'
-  const isPropuestaRoute = pathname.startsWith('/vista/propuesta/');
-
-  // Si el usuario no está autenticado y está intentando acceder a una vista de propuestas
-  if (!isAuthenticated(request) && isPropuestaRoute) {
-    if (isClientAuthenticated(request) && isPropuestaRoute) {
+    // Si la ruta es pública, permite el acceso
+    if (publicRoutes.includes(pathname)) {
       return NextResponse.next();
-      
     }
-    // Crear una respuesta de redirección
-    const response = NextResponse.redirect(new URL('/vista/propuesta/login', request.url));
 
-    response.cookies.set({
-      name: 'redirect_to',
-      value: pathname,
-      httpOnly: false,
-      path: '/',
-    });
+    // Comprobar si la ruta comienza con 'vista/propuesta/'
+    const isPropuestaRoute = pathname.startsWith('/vista/propuesta/');
 
-    return response;
-    //return NextResponse.redirect(new URL('/vista/propuesta/login', request.url));
-  } 
+    // Si el usuario no está autenticado y está intentando acceder a una vista de propuestas
+    if (!isAuthenticated(request) && isPropuestaRoute) {
+      if (isClientAuthenticated(request) && isPropuestaRoute) {
+        return NextResponse.next();
+
+      }
+      // Crear una respuesta de redirección
+      const response = NextResponse.redirect(new URL('/vista/propuesta/login', request.url));
+
+      response.cookies.set({
+        name: 'redirect_to',
+        value: pathname,
+        httpOnly: false,
+        path: '/',
+      });
+
+      return response;
+      //return NextResponse.redirect(new URL('/vista/propuesta/login', request.url));
+    }
 
 
-  // Si el usuario no está autenticado y está intentando acceder a una ruta privada
-  if (!isAuthenticated(request) && !publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL('/auth/login', request.url));
+    // Si el usuario no está autenticado y está intentando acceder a una ruta privada
+    if (!isAuthenticated(request) && !publicRoutes.includes(pathname)) {
+      return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
+
+    // Si el usuario está autenticado y está intentando acceder a la página de login, redirigir
+    if (isAuthenticated(request) && pathname === '/auth/login') {
+      return NextResponse.redirect(new URL('/contactos', request.url));
+    }
+
+    // Si ninguna de las condiciones anteriores aplica, continúa
+    return NextResponse.next();
+  } catch (error) {
+    console.error('Error en el Middleware:', error);
+    return new Response('Error en el Middleware', { status: 500 });
   }
-
-  // Si el usuario está autenticado y está intentando acceder a la página de login, redirigir
-  if (isAuthenticated(request) && pathname === '/auth/login') {
-    return NextResponse.redirect(new URL('/contactos', request.url));
-  }
-
-  // Si ninguna de las condiciones anteriores aplica, continúa
-  return NextResponse.next();
 }
 
 
